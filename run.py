54d1be3a8e34e517e1f01ea81391ae641d809c9b
Andre Chung <andre.chung@unifr.ch>
2024-11-24 18:21:41 +0100

    feat(app) added register and login routes

--- run.py
+++ run.py
@@ -12,7 +12,6 @@ nest_asyncio.apply()
 async def main():
     app = await init_app()
     start_mqtt_listener(app)
-    #app.add_routes(routes)  # Add OAuth2 routes to the app
     web.run_app(app, port=8000)
 
 if __name__ == '__main__':
